586242d4cffa3f841b5897b44f54c9b267fcaf85
wedigitek <carlos.santos@wedigitek.com>
2017-03-28 23:30:09 -0300

    Create BootLauncher.java

--- src/android/BootLauncher.java
+++ src/android/BootLauncher.java
@@ -10,7 +10,7 @@ public class BootLauncher extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent serviceIntent = new Intent(context, com.wedigitek.pesquisasatisfacao.MainActivity.class);
+        Intent serviceIntent = new Intent(context, com.wedigitek.inquiry.MainActivity.class);
         serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(serviceIntent);
     }
